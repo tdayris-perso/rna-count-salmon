28e16cd06bac287083d54ec131f4b958a3d7acbc
tdayris-perso <tdayris@tutanota.de>
2020-11-25 15:17:01 +0100

    [fix] (Conda): missing possibilities

--- .github/workflows/setup.sh
+++ .github/workflows/setup.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 export SINGULARITY_VER=3.5.3
 
 if type conda > /dev/null; then exit 0; fi
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
-bash miniconda.sh -b -p miniconda
+wget https://repo.anaconda.com/archive/Anaconda3-2020.11-Linux-x86_64.sh -O anaconda.sh
+bash anaconda.sh -b -p anaconda3
 conda config --add channels defaults
 conda config --add channels conda-forge
 conda config --add channels bioconda
